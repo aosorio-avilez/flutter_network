f7810e4482263d3d5c4c764d1a580acc219b90ca
Alfonso Israel Osorio Avilez <aosorio.avilez@gmail.com>
2022-10-27 11:32:54 -0500

    Export missing files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 ## 0.0.1
 
 Initial version
+
+## 0.0.2
+
+Export missing files
+    - api_rest_client_mock.dart
+    - api_rest_result.dart
--- lib/flutter_network.dart
+++ lib/flutter_network.dart
@@ -6,6 +6,8 @@ import 'package:flutter_network/src/internal_providers.dart';
 import 'package:riverpod/riverpod.dart';
 
 export 'src/api_rest_client.dart';
+export 'src/api_rest_client_mock.dart';
+export 'src/api_rest_result.dart';
 
 final apiUrlProvider = Provider<String>((ref) {
   throw UnimplementedError();
